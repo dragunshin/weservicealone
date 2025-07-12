50068b47d524ed25a6e9b629cb870dd25c761120
Dragon <sikk337@naver.com>
2025-07-12 19:03:09 +0900

    Initial commit

---
+++ src/main/java/com/example/weservicealone/WeservicealoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.weservicealone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeservicealoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeservicealoneApplication.class, args);
+    }
+
+}
